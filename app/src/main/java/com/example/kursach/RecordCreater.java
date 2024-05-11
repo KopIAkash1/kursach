d7d3aa0ef6ba8f67d49e7e1292a7dc8de119d476
KopIAkash1 <andreymart01@gmail.com>
2024-05-11 19:20:50 +0300

    multi records added

--- app/src/main/java/com/example/kursach/RecordCreater.java
+++ app/src/main/java/com/example/kursach/RecordCreater.java
@@ -5,24 +5,41 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class RecordCreater extends AppCompatActivity {
     FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference groupRef = database.getReference("groups").child("Records");
+    DatabaseReference groupRef = database.getReference("RecordMass").child("Records");
     TextInputEditText headerRecord, textRecord, skillsRecord;
     Button backButton, addButton;
+    private long counter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_record);
+
+        groupRef.addValueEventListener(new ValueEventListener(){
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                counter = snapshot.getChildrenCount();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Handle error
+            }
+        });
         headerRecord = findViewById(R.id.headerRecord);
         textRecord = findViewById(R.id.textRecord);
         skillsRecord = findViewById(R.id.skillsRecord);
@@ -47,7 +64,8 @@ public class RecordCreater extends AppCompatActivity {
                 skills = skillsRecord.getText().toString();
                 ArrayList<String> skillsMass = new ArrayList<>(Arrays.asList(skills.split(",")));
                 Record record = new Record(header,text,skillsMass);
-                groupRef.setValue(record);
+                counter += 1;
+                groupRef.child("Record " + counter).setValue(record);
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
                 finish();
